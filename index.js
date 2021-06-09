926b0b898ff19a53fe72a5777dd2a399c096ea94
akashmehrotra29 <>
2021-06-09 06:38:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to capewatch')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});

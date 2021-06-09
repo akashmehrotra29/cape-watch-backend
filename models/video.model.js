d51c3f6e63f77a9ca522de67af9f0cc715d7d681
Akash Mehrotra <50659075+akashmehrotra29@users.noreply.github.com>
2021-06-09 20:44:28 +0000

    feat: created models

---
+++ models/video.model.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+require('mongoose-type-url');
+
+const VideoSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: "Can't add without video id"
+  },
+  thumbnailUrl: {
+    type: String,
+    required: "Can't add without thumbnailURL"
+  },
+  title: {
+    type: String,
+    required: "Can't add without title"
+  },
+  description: {
+    type: String,
+    required: "Can't add without description"
+  },
+  categories: [{
+    type: String
+  }],
+  channelName: {
+    type: String,
+    required: "Can't add without channel name"
+  },
+  channelImageURL: {
+    type: mongoose.SchemaTypes.URL,
+    required: true
+  },
+  publishedDate: {
+    type: String,
+    required: "Can't add without published date"
+  },
+  viewCount: {
+    type: Number,
+    required: "Can't add without view count"
+  },
+  likeCount: {
+    type: Number,
+    required: "Can't add without like count"
+  },
+  dislikeCount: {
+    type: Number,
+    required: "Can't add without dislike count"
+  },
+  commentCount: {
+    type: Number,
+    required: "Can't add without comment count"
+  }
+}, { timestamps: true });
+
+const Video = mongoose.model("Video", VideoSchema);
+module.exports = { Video }

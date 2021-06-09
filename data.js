ab443db6a1e29523771d47e56ecb043f6363bd62
akashmehrotra29 <>
2021-06-09 06:45:42 +0000

    feat: added videos data

---
+++ data.js
@@ -0,0 +1,120 @@
+exports.videos = [
+  {
+    id: "_L-UszPmy2A",
+    thumbnailURL: "https://i3.ytimg.com/vi/_L-UszPmy2A/maxresdefault.jpg",
+    title: "First CLI App: Intro to programming",
+    description:
+      "Welcome to neogcamp's first class. This is the most basic class you'll find on programming. We will use JavaScript to code. Using nodeJS we will write a CLI (Command Line Interface) program. We'll then use https://repl.it​ to host it.For intermediate to advanced developers, this class would be too basic. Therefore, you can skip this and directly make the app for the camp's submission for levelOne.",
+    categories: ["Node Js", "Web Dev"],
+    channelName: "Tanay Pratap",
+    channelImageURL:
+      "https://yt3.ggpht.com/ytc/AAUvwnibnmzbi8nfRHEAzdI-8lpPGsrD1F6Cg3mAImtQ=s88-c-k-c0x00ffffff-no-rj",
+    publishedDate: "14-Nov-2020",
+    statistics: {
+      viewCount: 38662,
+      likeCount: 1000,
+      dislikeCount: 19,
+      commentCount: 94
+    }
+  },
+
+  {
+    id: "MMEIVh49pS8",
+    thumbnailURL: "https://i3.ytimg.com/vi/MMEIVh49pS8/maxresdefault.jpg",
+    title: "Getting started with Git, VSCode and Hosting",
+    description:
+      "Welcome to neogcamp's second class. For a newbie to self host his/her first website and program more apps we need some tooling to be in place. Tools help you make your life easier. All these tools are free for you and will give you a better place to code, a place to store your code in the cloud so that you don't lose it, and then a place to host your app so that others can find it. This session will set you up for the next class where we'll develop our very own portfolio website and blog. The blog part will be an assignment but I'll help you with that.",
+    categories: ["Node Js", "Web Dev"],
+    channelName: "Tanay Pratap",
+    channelImageURL:
+      "https://yt3.ggpht.com/ytc/AAUvwnibnmzbi8nfRHEAzdI-8lpPGsrD1F6Cg3mAImtQ=s88-c-k-c0x00ffffff-no-rj",
+
+    publishedDate: "21-Nov-2020",
+    statistics: {
+      viewCount: 19298,
+      likeCount: 934,
+      dislikeCount: 15,
+      commentCount: 94
+    }
+  },
+
+  {
+    id: "No8qdcVYiQw",
+    thumbnailURL: "https://i3.ytimg.com/vi/No8qdcVYiQw/maxresdefault.jpg",
+    title: "Getting started with HTML5",
+    description:
+      "Welcome to neogcamp's levelZero journey. You don't know anything about web development? Good, you have come to the right place. Not only you'll learn the language of the web: HTML, you'll learn how to make your site pretty with CSS. When we have learned everything we will put together a decent profile to host your Internet adventures. ",
+    categories: ["Node Js", "Web Dev"],
+    channelName: "Tanay Pratap",
+    channelImageURL:
+      "https://yt3.ggpht.com/ytc/AAUvwnibnmzbi8nfRHEAzdI-8lpPGsrD1F6Cg3mAImtQ=s88-c-k-c0x00ffffff-no-rj",
+
+    publishedDate: "22-Nov-2020",
+    statistics: {
+      viewCount: 15771,
+      likeCount: 579,
+      dislikeCount: 12,
+      commentCount: 94
+    }
+  },
+
+  {
+    id: "u6_a0d94A1Q",
+    thumbnailURL: "https://i3.ytimg.com/vi/u6_a0d94A1Q/maxresdefault.jpg",
+    title: "Getting started with CSS3",
+    description:
+      "Welcome to neogcamp's levelZero journey. You don't know anything about web development? Good, you have come to the right place. Not only you'll learn the language of the web: HTML, you'll learn how to make your site pretty with CSS. When we have learned everything we will put together a decent profile to host your Internet adventures. The assignment for this session is to add a blog section to your freshly created portfolio.",
+    categories: ["Node Js", "Web Dev"],
+    channelName: "Tanay Pratap",
+    channelImageURL:
+      "https://yt3.ggpht.com/ytc/AAUvwnibnmzbi8nfRHEAzdI-8lpPGsrD1F6Cg3mAImtQ=s88-c-k-c0x00ffffff-no-rj",
+
+    publishedDate: "22-Nov-2020",
+    statistics: {
+      viewCount: 16831,
+      likeCount: 427,
+      dislikeCount: 3,
+      commentCount: 94
+    }
+  },
+
+  {
+    id: "yLZazznWoAs",
+    thumbnailURL: "https://i3.ytimg.com/vi/yLZazznWoAs/maxresdefault.jpg",
+    title: "JavaScript in the browser!",
+    description:
+      "Welcome to neogcamp. We have learned programming and web basics in previous lessons. This time we will use both things to create a dynamic app in a web browser. You'll learn about DOM scripting i.e. JS in the browser, calling servers, and getting data from there, taking user input and showing user output, and many more things.",
+    categories: ["Node Js", "Web Dev"],
+    channelName: "Tanay Pratap",
+    channelImageURL:
+      "https://yt3.ggpht.com/ytc/AAUvwnibnmzbi8nfRHEAzdI-8lpPGsrD1F6Cg3mAImtQ=s88-c-k-c0x00ffffff-no-rj",
+
+    publishedDate: "28-Nov-2020",
+    statistics: {
+      viewCount: 18085,
+      likeCount: 750,
+      dislikeCount: 6,
+      commentCount: 94
+    }
+  },
+
+  {
+    id: "KUJsaM-hAjs",
+    thumbnailURL: "https://i3.ytimg.com/vi/KUJsaM-hAjs/maxresdefault.jpg",
+    title: "JavaScript in the browser!",
+    description:
+      "Welcome to neogcamp. We have learned programming, web basics and vanillaJS in previous lessons. Now we use ReactJS, the most in-demand JS library today to create a dynamic app in the browser. We will be looking at the basics of React, JSX, useState, onClick handler, etc. ",
+    categories: ["Node Js", "Web Dev"],
+    channelName: "Tanay Pratap",
+    channelImageURL:
+      "https://yt3.ggpht.com/ytc/AAUvwnibnmzbi8nfRHEAzdI-8lpPGsrD1F6Cg3mAImtQ=s88-c-k-c0x00ffffff-no-rj",
+
+    publishedDate: "06-Dec-2020",
+    statistics: {
+      viewCount: 14985,
+      likeCount: 652,
+      dislikeCount: 1,
+      commentCount: 94
+    }
+  }
+];

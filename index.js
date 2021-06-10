7807dab0e62852d861faa8c68c12c335dd93210b
Akash Mehrotra <50659075+akashmehrotra29@users.noreply.github.com>
2021-06-10 08:56:09 +0000

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to capewatch')
+  res.send(' Welcome to capewatch ')
 });
 
 app.listen(3000, () => {
